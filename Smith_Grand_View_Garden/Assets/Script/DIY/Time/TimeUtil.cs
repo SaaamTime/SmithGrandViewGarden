425b8fbf4d8b3017482620f0fb3b109870e3fb08
SaaamTime <saaamtime@qq.com>
2021-02-18 00:21:43 +0800

    Create TimeUtil.cs

---
+++ Smith_Grand_View_Garden/Assets/Script/DIY/Time/TimeUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIY.Time
+{
+    public static class  TimeUtil
+    {
+        public static long GetTimestamp() {
+            return DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            //return (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000;
+        }
+    }
+}
