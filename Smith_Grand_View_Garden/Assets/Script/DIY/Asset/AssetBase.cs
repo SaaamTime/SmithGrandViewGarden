9482adcd1323756c5f131838b70ebd42b388adcb
SaaamTime <saaamtime@qq.com>
2021-02-17 22:49:41 +0800

    Create AssetBase.cs

---
+++ Smith_Grand_View_Garden/Assets/Script/DIY/Asset/AssetBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIY.Asset
+{
+    /// <summary>
+    /// 资源基类，默认所有对象都是资源
+    /// </summary>
+    public class AssetBase
+    {
+        public string path;
+        public string name;
+
+      
+    }
+}
