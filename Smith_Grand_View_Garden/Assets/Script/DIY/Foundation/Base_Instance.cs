4c1ba232edb201e61834992497d18946ccf72aca
SaaamTime <saaamtime@qq.com>
2021-02-18 00:21:36 +0800

    Create Base_Instance.cs

---
+++ Smith_Grand_View_Garden/Assets/Script/DIY/Foundation/Base_Instance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DIY.Foundation
+{
+    public abstract class Base_Instance<T> where T:new()
+    {
+        private static T _instance;
+        public static T Instance {
+            get {
+                if (null == _instance)
+                {
+                    _instance = new T();
+                }
+                return _instance;
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
